b443026ffeff7a9aa2637a3f8c0dc8bea46f8e37
GabrielKameoka <gabrielkameoka@gmail.com>
2024-11-05 16:18:06 -0300

    finalizando estilização

--- EmprestimoLivros/obj/Debug/net8.0/EmprestimoLivros.AssemblyInfo.cs
+++ EmprestimoLivros/obj/Debug/net8.0/EmprestimoLivros.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+34592f066a26a2b1203b6f136c66dec01c27bac5")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmprestimoLivros")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
